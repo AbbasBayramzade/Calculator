f6e619ee6d5f28c188cb6ad2a046fcbfe6de4a43
AbbasBayramzade <abbas.bayramzade@mail.ru>
2023-10-18 16:36:37 +0400

    I created Calculator in React

---
+++ src/components/numbers/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import '../numbers/index.scss'
+
+const Numbers = () => {
+
+   const arr = [1,2,3,"+",4,5,6,"-",7,8,9,"*","C",0,"/ ","=" ]
+   const [yazi, setYazi] = useState("")
+
+   const handleButtonClick = (value) =>{
+
+     if(value=="C"){
+
+        setYazi("")
+
+     }else if(value ==="="){
+
+        try {
+            const result = eval(yazi); // Use JavaScript's eval function to evaluate the expression
+            setYazi(result.toString()); // Display the result
+          } catch (error) {
+            setYazi("Error"); // Handle invalid expressions
+          }
+          
+     }else{
+        setYazi(yazi+value)
+     }
+
+   }
+
+  return (
+    <div id='home_div'>
+        <div id='result_div'>
+           <div>{yazi}</div> 
+        </div>
+     <div id='calc_div'>
+       {arr.map((elem,i)=>{
+         return <button key={i} onClick={()=>handleButtonClick(elem)}>{elem}</button>
+       })} 
+     </div>
+    </div>
+  )
+}
+
+export default Numbers
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Numbers from '../../components/numbers'
+
+const Home = () => {
+  return (
+    <div>
+      <Numbers/>
+    </div>
+  )
+}
+
+export default Home
